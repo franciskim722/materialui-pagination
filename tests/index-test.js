6e74ed0498e86086c7e7185a56256169517e6f7e
Francis Kim <franciskim722@gmail.com>
2018-03-11 22:35:37 -0700

    + Test

--- tests/index-test.js
+++ tests/index-test.js
@@ -1,23 +1,42 @@
-import expect from 'expect'
-import React from 'react'
-import {render, unmountComponentAtNode} from 'react-dom'
+import expect from 'expect';
+import React from 'react';
 
-import Component from 'src/'
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-15';
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+Enzyme.configure({ adapter: new Adapter() });
+
+import Pagination from 'src/';
+
+const mockProps = {
+  total: 0,
+  page: 1,
+  rowsPerPage: [10, 20, 30],
+  numberOfRows: 10,
+  pageTitle: 'Page:',
+  rowsPerPageTitle: 'Rows Per Page:',
+  prepositionForRowRange: 'of',
+  updateRows: () => {return "Update Rows"}
+};
 
 describe('Component', () => {
-  let node
-
-  beforeEach(() => {
-    node = document.createElement('div')
-  })
-
-  afterEach(() => {
-    unmountComponentAtNode(node)
-  })
-
-  it('displays a welcome message', () => {
-    render(<Component/>, node, () => {
-      expect(node.innerHTML).toContain('Welcome to React components')
-    })
-  })
-})
+  const wrapper = Enzyme.mount(
+    <MuiThemeProvider>
+      <Pagination
+        {...mockProps}
+      />
+    </MuiThemeProvider>).children();
+  it('Maps props and default props correctly', () => {
+    console.log()
+    expect(wrapper.props().total).toEqual(0);
+    expect(wrapper.props().page).toEqual(1);
+    expect(wrapper.props().numberOfRows).toEqual(10);
+    expect(wrapper.props().pageTitle).toEqual('Page:');
+    expect(wrapper.props().rowsPerPageTitle).toEqual('Rows Per Page:');
+    expect(wrapper.props().prepositionForRowRange).toEqual('of');
+    expect(wrapper.props().updateRows()).toEqual("Update Rows");
+  });
+});
